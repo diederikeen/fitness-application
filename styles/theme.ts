545fb635e3ad281171cc9488c3e9c2fa3960d0db
diederikeenschooten <diederikeen@gmail.com>
2023-01-05 08:13:58 -0500

    feature: initialize theme

---
+++ styles/theme.ts
@@ -0,0 +1,116 @@
+import { createStitches } from "@stitches/react";
+
+export const { styled, css } = createStitches({
+  theme: {
+    colors: {
+      white: "#fff",
+      primaryBg: "#fff",
+      secondaryBg: "#eee",
+      primaryColor: "#7ed6df",
+    },
+    radii: {
+      0: "2px",
+      1: "3px",
+      2: "4px",
+      4: "6px",
+      5: "8px",
+      6: "10px",
+      full: "50%",
+    },
+    space: {
+      0: "2px",
+      1: "4px",
+      2: "8px",
+      3: "12px",
+      4: "16px",
+      5: "32px",
+      6: "64px",
+      7: "76px",
+      8: "96px",
+    },
+    fontSizes: {
+      1: "6px",
+      2: "8px",
+      3: "12px",
+      4: "16px",
+      5: "22px",
+      6: "28px",
+      7: "36px",
+      8: "52px",
+    },
+    fontWeights: {
+      0: "100",
+      1: "300",
+      2: "500",
+      3: "600",
+      4: "800",
+    },
+    lineHeights: {
+      0: 0.8,
+      1: 1,
+      2: 1.2,
+      3: 1.4,
+      4: 1.6,
+      5: 1.8,
+    },
+    shadows: {
+      0: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+      1: "rgba(99, 99, 99, 0.1) 0px 2px 12px 0px",
+    },
+  },
+  media: {
+    bp1: "(min-width: 480px)",
+    bp2: "(min-width: 768px)",
+    bp3: "(min-width: 1024px)",
+    bp4: "(min-width: 1440px)",
+  },
+  utils: {
+    // Abbreviated margin properties
+    m: (value: string | number) => ({
+      margin: value,
+    }),
+    mt: (value: string | number) => ({
+      marginTop: value,
+    }),
+    mr: (value: string | number) => ({
+      marginRight: value,
+    }),
+    mb: (value: string | number) => ({
+      marginBottom: value,
+    }),
+    ml: (value: string | number) => ({
+      marginLeft: value,
+    }),
+    mx: (value: string | number) => ({
+      marginLeft: value,
+      marginRight: value,
+    }),
+    my: (value: string | number) => ({
+      marginTop: value,
+      marginBottom: value,
+    }),
+    p: (value: string | number) => ({
+      padding: value,
+    }),
+    pt: (value: string | number) => ({
+      paddingTop: value,
+    }),
+    pr: (value: string | number) => ({
+      paddingRight: value,
+    }),
+    pb: (value: string | number) => ({
+      paddingBottom: value,
+    }),
+    pl: (value: string | number) => ({
+      paddingLeft: value,
+    }),
+    px: (value: string | number) => ({
+      paddingLeft: value,
+      paddingRight: value,
+    }),
+    py: (value: string | number) => ({
+      paddingTop: value,
+      paddingBottom: value,
+    }),
+  },
+});
