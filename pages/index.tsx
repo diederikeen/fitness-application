988543dd5a45133d16c0edae651a30ec75c96bd7
diederikeenschooten <diederikeen@gmail.com>
2023-01-10 00:04:03 +0100

    feature: Create connection with Firebase and write user in Firebase database

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import * as Yup from "yup";
 
 import { styled } from "../styles/theme";
@@ -52,38 +52,51 @@ export default function HomePage() {
     validateOnBlur: true,
     validateOnChange: false,
     validationSchema: signUpValidationSchema,
-    onSubmit: async (values) => await onSubmit(),
+    onSubmit: async (values) => await onSubmit(values),
   });
 
-  async function onSubmit() {
-    // return await signInWithEmailAndPassword(auth, email, password)
-    //   .then((userCredential) => {
-    //     // Signed in
-    //     const user = userCredential.user;
-    //     // ...
-    //   })
-    //   .catch((error) => {
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-    //   });
+  async function onSubmit({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) {
+    return await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // @TODO: Create api call to own database and write user
+        // in here we create a new user in our own database
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        // @TODO: Create notification component
+        // in here we create a new user in our own database
+        console.log({ errorCode, errorMessage });
+      });
   }
 
   return (
     <Layout>
       <div className={"content"}>
-        <Box css={{ width: "100%", mb: "$5" }}>
-          <h1>Sign up</h1>
+        <Box css={{ maxWidth: "600px" }}>
+          <Box css={{ width: "100%", mb: "$5" }}>
+            <h1>Sign up</h1>
 
-          <p>
-            Once you've created an account, you'll be able to track your
-            workouts, set fitness goals, and connect with friends for added
-            motivation. You'll also have access to a variety of workouts and
-            exercises tailored to your fitness level and interests.
-          </p>
+            <p>
+              Once you've created an account, you'll be able to track your
+              workouts, set fitness goals, and connect with friends for added
+              motivation. You'll also have access to a variety of workouts and
+              exercises tailored to your fitness level and interests.
+            </p>
+          </Box>
+          <FormikProvider value={formik}>
+            <FormComposer fields={signupFormFields} buttonLabel={"Sign up"} />
+          </FormikProvider>
         </Box>
-        <FormikProvider value={formik}>
-          <FormComposer fields={signupFormFields} buttonLabel={"Sign up"} />
-        </FormikProvider>
       </div>
       <VisualSection>
         <div className={"content"}></div>
