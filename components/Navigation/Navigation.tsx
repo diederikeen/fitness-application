0ca0b19cf87c3728b599bd416756f4c2f4db557d
diederikeenschooten <diederikeen@gmail.com>
2023-01-31 13:12:44 +0100

    feature: add navigation + navigation styling

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,8 +1,76 @@
 import { styled } from "../../styles/theme";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import { UilDashboard, UilWeight } from "@iconscout/react-unicons";
+
+const NAV_ITEMS = [
+  {
+    path: "/dashboard",
+    icon: <UilDashboard />,
+    label: "Dashboard",
+  },
+  {
+    path: "/weight-tracker",
+    icon: <UilWeight />,
+    label: "Weight Tracker",
+  },
+];
 
 function Navigation() {
-  return <StyledNavigation />;
+  const { asPath } = useRouter();
+
+  return (
+    <StyledNavigation>
+      {NAV_ITEMS.map((item) => (
+        <StyledNavItem
+          key={item.path}
+          href={item.path}
+          isCurrent={asPath === item.path}
+        >
+          {item.icon} {item.label}
+        </StyledNavItem>
+      ))}
+    </StyledNavigation>
+  );
 }
+
+const StyledNavItem = styled(Link, {
+  background: "red",
+  borderRadius: "$3",
+  height: "50px",
+  display: "flex",
+  alignItems: "center",
+  px: "$3",
+  fontSize: "$3",
+  fontWeight: "$3",
+  backgroundColor: "$grey100",
+  transition: "background, 125ms ease",
+  textDecoration: "none",
+  color: "$textDefault",
+
+  ">svg": {
+    mr: "$2",
+  },
+
+  "&:hover": {
+    backgroundColor: "$grey300",
+    color: "$white",
+  },
+
+  "&:not(:last-child)": {
+    mb: "$3",
+  },
+
+  variants: {
+    isCurrent: {
+      true: {
+        backgroundColor: "$primaryColor",
+        color: "$white",
+      },
+    },
+  },
+});
 
 const StyledNavigation = styled("nav", {
   display: "none",
@@ -12,7 +80,6 @@ const StyledNavigation = styled("nav", {
   background: "white",
   py: "$3",
   px: "$4",
-  // backgroundColor: "$grey100",
   borderRight: "1px solid $grey300",
   "@bp2": {
     display: "block",
