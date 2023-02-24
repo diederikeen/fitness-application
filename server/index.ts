069df1bf0b43e192e38b2e44c04dee3035700adc
diederikeenschooten <diederikeen@gmail.com>
2023-02-24 19:11:47 +0100

    feature: create new post user route

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import user from "./routes/user";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const PORT = 8080;
 const app = express();
 
+// Middlewares
 app.use(cookieParser());
+app.use(bodyParser.json());
 
 // Routes
 app.use('/user', user);
--- server/routes/user/index.ts
+++ server/routes/user/index.ts
@@ -1,4 +1,6 @@
 import {Router} from "express";
+import z from "zod";
+
 import {prisma} from "../../prisma";
 import {firebaseAdmin} from "../../firebase";
 
@@ -31,5 +33,42 @@ router.get('/', async (req, res) => {
     })
   }
 });
+
+const createUserPayloadSchema = z.object({
+  userDetails: z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+    zipcode: z.string(),
+    country: z.string(),
+    city: z.string(),
+    streetName: z.string(),
+  }),
+  user: z.object({
+    uid: z.string(),
+    photoUrl: z.string().nullable(),
+    email: z.string(),
+  }),
+});
+
+router.post('/', async (req, res) => {
+  const {userDetails, user} = createUserPayloadSchema.parse(req.body);
+
+  const createdUser = prisma.user.create({
+    data: {
+      ...userDetails,
+      ...user,
+    },
+    select: {
+      photoUrl: true,
+      firstName: true,
+      lastName: true,
+      email: true,
+    },
+  })
+
+
+  return res.json({user: createdUser})
+
+})
 
 export default router;
