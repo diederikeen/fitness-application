65f6b4f9425599a753d0ba3b6a50297bbcf74004
diederikeenschooten <diederikeen@gmail.com>
2023-02-24 23:07:24 +0100

    feature: add folder endpoints

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 
 import user from "./routes/user";
 import weight from "./routes/weight";
+import folders from "./routes/folders";
 
 const PORT = 8080;
 const app = express();
@@ -15,5 +16,6 @@ app.use(bodyParser.json());
 // Routes
 app.use('/user', user);
 app.use('/weight', weight);
+app.use('/folders', folders);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
---
+++ server/routes/folders/index.ts
@@ -0,0 +1,82 @@
+import {Router} from "express";
+import z from "zod";
+
+import {authenticateUser} from "../../middleware/auth";
+import {prisma} from "../../prisma";
+
+const router = Router();
+
+router.use(authenticateUser);
+
+router.get('/', async (req, res) => {
+  const uid = res.locals.user.uid;
+
+  try {
+    const folders = await prisma.folder.findMany({
+      where: {
+        uid: uid,
+      },
+      select: {
+        id: true,
+        name: true,
+        exercises: true,
+      },
+    });
+    return res.status(200).json({folders});
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({message: "Something went wrong with fetching your folder"});
+  }
+});
+
+router.post('/', async (req, res) => {
+  const uid = res.locals.user.uid;
+
+  const {name} = postFolderScheme.parse(req.body);
+
+  try {
+    await prisma.folder.create({
+      data: {
+        uid: uid,
+        name: name,
+      },
+    });
+
+    return res.status(200).json({message: "Folder created successfully"});
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({message: "Something went wrong with creating your folder"});
+  }
+});
+
+const postFolderScheme = z.object({
+  name: z.string(),
+});
+
+
+router.get('/single', async (req, res) => {
+  const {id} = getSingleFolderSchema.parse(req.body);
+
+  try {
+    const folder = await prisma.folder.findUnique({
+      where: {
+        id: parseInt(id, 10),
+      },
+      select: {
+        id: true,
+        name: true,
+        exercises: true,
+      },
+    });
+    return res.status(200).json({folder});
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({message: "Something went wrong fetching your folder"})
+  }
+})
+
+const getSingleFolderSchema = z.object({
+  id: z.string(),
+})
+
+export default router;
